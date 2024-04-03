433dac76c1a79ebce18182e9c5433997892a6db4
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-03 16:16:50 +0300

    clean up

--- src/c_files/is_dead_full.c
+++ src/c_files/is_dead_full.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:15:10 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 15:11:57 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:08:15 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	ft_is_dead_full(void *input)
 {
-	int	error;
 	int	end;
 
 	end = 0;
-	error = pthread_mutex_lock(((((t_input *)input)->info)->start_lock));
+	if (pthread_mutex_lock(((((t_input *)input)->info)->start_lock)))
+		return (1);
 	if (*((((t_input *)input)->info)->dead) == 1)
 		end = 1;
 	else if (*((((t_input *)input)->info)->full) == 1)
 		end = 1;
-	error = pthread_mutex_unlock(((((t_input *)input)->info)->start_lock));
+	if (pthread_mutex_unlock(((((t_input *)input)->info)->start_lock)))
+		return (1);
 	return (end);
 }
--- src/c_files/lock_print.c
+++ src/c_files/lock_print.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 12:54:59 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 16:00:39 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:13:52 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,59 @@
 
 int	ft_lock_print(long long timestamp, int number, char *str, t_info *info)
 {
-	int			error;
-
-	error = 0;
-	error = pthread_mutex_lock(info->print_lock);
-	error = pthread_mutex_lock(info->start_lock);
+	if (pthread_mutex_lock(info->print_lock))
+		return (1);
+	if (pthread_mutex_lock(info->start_lock))
+		return (1);
 	if (*(info->dead) == 1)
 	{
-		error = pthread_mutex_unlock(info->start_lock);
-		error = pthread_mutex_unlock(info->print_lock);
-		return (error);
+		if (pthread_mutex_unlock(info->start_lock))
+			return (1);
+		if (pthread_mutex_unlock(info->print_lock))
+			return (1);
 	}
 	else
 	{
-		error = pthread_mutex_unlock(info->start_lock);
+		if (pthread_mutex_unlock(info->start_lock))
+			return (1);
 		printf("%lld\t%d %s\n", timestamp, number, str);
-		error = pthread_mutex_unlock(info->print_lock);
-		return (error);
+		if (pthread_mutex_unlock(info->print_lock))
+			return (1);
 	}
+	return (0);
 }
 
 int	ft_lock_print_eating(long long timestamp, int number, t_info *info)
 {
-	int			error;
-
-	error = 0;
-	error = pthread_mutex_lock(info->print_lock);
-	error = pthread_mutex_lock(info->start_lock);
+	if (pthread_mutex_lock(info->print_lock))
+		return (1);
+	if (pthread_mutex_lock(info->start_lock))
+		return (1);
 	if (*(info->dead) == 1)
 	{
-		error = pthread_mutex_unlock(info->start_lock);
-		error = pthread_mutex_unlock(info->print_lock);
-		return (error);
+		if (pthread_mutex_unlock(info->start_lock))
+			return (1);
+		if (pthread_mutex_unlock(info->print_lock))
+			return (1);
 	}
 	else
 	{
-		error = pthread_mutex_unlock(info->start_lock);
+		if (pthread_mutex_unlock(info->start_lock))
+			return (1);
 		printf("%lld\t%d %s\n", timestamp, number, "has taken a fork");
 		printf("%lld\t%d %s\n", timestamp, number, "is eating");
-		error = pthread_mutex_unlock(info->print_lock);
-		return (error);
+		if (pthread_mutex_unlock(info->print_lock))
+			return (1);
 	}
+	return (0);
 }
 
 int	ft_lock_print_dead(long long timestamp,	int number, t_info *info)
 {
-	int			error;
-
-	error = 0;
-	error = pthread_mutex_lock(info->print_lock);
+	if (pthread_mutex_lock(info->print_lock))
+		return (1);
 	printf("\033[1;31m%lld\t%d %s\033[0m\n", timestamp, number, "is dead");
-	error = pthread_mutex_unlock(info->print_lock);
-	return (error);
+	if (pthread_mutex_unlock(info->print_lock))
+		return (1);
+	return (0);
 }
