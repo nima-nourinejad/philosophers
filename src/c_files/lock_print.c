bb4e245d7dbc0f781f81cc4e0eacd5c83ef844bc
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-03 13:14:36 +0300

    after lock the print lock I check for death. this way i make sure when i lock the print lock i am ot using the wrong dead variabe

--- src/c_files/lock_print.c
+++ src/c_files/lock_print.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 12:54:59 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 12:14:55 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/03 13:09:36 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@ int	ft_lock_print(long long timestamp, int number, char *str, t_info *info)
 	int			error;
 
 	error = 0;
+	error = pthread_mutex_lock(info->print_lock);
 	error = pthread_mutex_lock(info->start_lock);
 	if (*(info->dead) == 1)
 	{
 		error = pthread_mutex_unlock(info->start_lock);
+		error = pthread_mutex_unlock(info->print_lock);
 		return (error);
 	}
 	else
+	{
 		error = pthread_mutex_unlock(info->start_lock);
-	error = pthread_mutex_lock(info->print_lock);
-	printf("%lld\t%d %s\n", timestamp, number, str);
-	error = pthread_mutex_unlock(info->print_lock);
-	return (error);
+		printf("%lld\t%d %s\n", timestamp, number, str);
+		error = pthread_mutex_unlock(info->print_lock);
+		return (error);
+	}
 }
 
 int	ft_lock_print_dead(long long timestamp,	int number, char *str, t_info *info)
