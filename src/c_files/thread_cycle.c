4b3cceeff0060f4ce24dd18f864e84584cd95692
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-03 11:08:07 +0300

    works for 400 800 200 200

--- src/c_files/thread_cycle.c
+++ src/c_files/thread_cycle.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 13:36:31 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 10:13:19 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/03 11:04:44 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,9 @@ void	*ft_philo_cycle(void *input)
 	while (!finish)
 	{
 		if (thread_num % 2 == 0)
-			ft_only_wait_ms(5);
-		if (thread_num == total_number
-			&& total_number % 2 != 0 && total_number > 1)
-			ft_only_wait_ms(10);
+			usleep(5000);
+		else if (thread_num == total_number && total_number > 1)
+			usleep(10000);
 		finish = ft_eat_sleep(input, thread_num, finish);
 		finish = ft_think(input, thread_num, finish);
 	}
