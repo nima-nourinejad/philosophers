397364c863fd135fefb54365cf3b44ed40b45656
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-03 14:34:50 +0300

    odds philo (except the last one) do not go to think at begening

--- src/c_files/thread_cycle.c
+++ src/c_files/thread_cycle.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 13:36:31 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 13:31:40 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/03 14:30:02 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,18 @@ void	*ft_philo_cycle(void *input)
 	finish = 0;
 	total_number = *(((((t_input *)input)->info)->data)->value);
 	thread_num = *(((t_input *)input)->thread_num);
-	finish = ft_think(input, thread_num, finish);
 	if (total_number == 1)
 		return (ft_eat_one(input));
 	if (thread_num % 2 == 0)
+	{
+		finish = ft_think(input, thread_num, finish);
 		usleep(3000);
+	}
 	else if (thread_num == total_number)
+	{
+		finish = ft_think(input, thread_num, finish);
 		usleep(6000);
+	}
 	while (!finish)
 	{
 		finish = ft_eat_sleep(input, thread_num, finish);
