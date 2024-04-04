be22c28af424e1c26242e361db181767b3c973b3
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-04 13:55:02 +0300

    clean up of eat one

--- src/c_files/eat_one.c
+++ src/c_files/eat_one.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 13:24:06 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/04 11:58:57 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:53:55 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@ void	*ft_eat_one(void *input)
 {
 	long long	t;
 	long long	start_time;
-	int			error;
 	t_philo		*philo_node;
 
 	start_time = *((((t_input *)input)->info)->start_time);
 	t = ft_timestamp_ms() - start_time;
 	philo_node = (((t_input *)input)->info)->philo;
-	error = pthread_mutex_lock(philo_node->left_fork);
-	if (!error)
-	{
-		t = ft_timestamp_ms() - start_time;
-		ft_lock_print(t, 1, "has taken a fork", ((t_input *)input)->info);
-	}
-	pthread_mutex_unlock(philo_node->left_fork);
+	if (pthread_mutex_lock(philo_node->left_fork))
+		return (0);
+	t = ft_timestamp_ms() - start_time;
+	ft_lock_print(t, 1, "has taken a fork", ((t_input *)input)->info);
+	if (pthread_mutex_unlock(philo_node->left_fork))
+		return (0);
 	return (0);
 }
--- src/c_files/think.c
+++ src/c_files/think.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:46:10 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/04 10:52:33 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:51:40 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	ft_think(void *input, int thread_num, int finish)
 	t = ft_timestamp_ms() - start_time;
 	if (ft_is_dead_full(input))
 		return (1);
-	if (ft_lock_print(t, thread_num, "is thinking", ((t_input *)input)->info))
-		return (2);
+	ft_lock_print(t, thread_num, "is thinking", ((t_input *)input)->info);
 	return (0);
 }
