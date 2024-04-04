657fc16671342379d2a65d28634dcb02d7fe694f
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-04 14:17:43 +0300

    clean up of eat sleep. added first fork fuction

--- src/c_files/eat_sleep.c
+++ src/c_files/eat_sleep.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:01:15 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/03 15:19:58 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/04 14:13:20 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	ft_sleep(void *input, int thread_num)
 {
 	long long	t;
 	long long	start_time;
-	int			error;
 	t_philo		*philo_node;
 	int			sleep_time;
 
@@ -27,61 +26,65 @@ int	ft_sleep(void *input, int thread_num)
 		philo_node = philo_node->next;
 	sleep_time = *(((((((t_input *)input)->info)
 						->data)->next)->next)->next->value);
-	ft_check_error(pthread_mutex_unlock(philo_node->left_fork),
-		&error, "unlock problem");
-	ft_check_error(pthread_mutex_unlock(philo_node->right_fork),
-		&error, "unlock problem");
+	if (pthread_mutex_unlock(philo_node->left_fork))
+		return (1);
+	if (pthread_mutex_unlock(philo_node->right_fork))
+		return (1);
 	ft_lock_print(t, thread_num, "is sleeping", ((t_input *)input)->info);
 	ft_wait_ms(sleep_time, t, start_time);
 	if (ft_is_dead_full(input))
 		return (1);
-	return (error);
+	return (0);
+}
+
+int	ft_first_fork(t_philo *philo_node, int thread_num,
+	t_info *info, void *input)
+{
+	long long	t;
+	long long	start_time;
+
+	start_time = *(info->start_time);
+	t = ft_timestamp_ms() - start_time;
+	if (ft_is_dead_full(input))
+		return (1);
+	if (pthread_mutex_lock(philo_node->left_fork))
+		return (2);
+	t = ft_timestamp_ms() - start_time;
+	ft_lock_print(t, thread_num, "has taken a fork", info);
+	if (ft_is_dead_full(input))
+	{
+		if (pthread_mutex_unlock(philo_node->left_fork))
+			return (2);
+		return (1);
+	}
+	return (0);
 }
 
 int	ft_eat_sleep(void *input, int thread_num, int finish)
 {
 	long long	t;
 	long long	start_time;
-	int			error;
 	t_philo		*philo_node;
 	int			eat_time;
-	int			eat_times;
 
-	start_time = *((((t_input *)input)->info)->start_time);
-	t = ft_timestamp_ms() - start_time;
 	philo_node = (((t_input *)input)->info)->philo;
+	start_time = *((((t_input *)input)->info)->start_time);
 	while (*(philo_node->philo_num) != thread_num)
 		philo_node = philo_node->next;
 	eat_time = *((((((t_input *)input)->info)->data)->next)->next->value);
-	eat_times = *((((((((t_input *)input)->info)->data)
-						->next)->next)->next)->next->value);
-	if (ft_is_dead_full(input))
+	if (ft_first_fork(philo_node, thread_num, ((t_input *)input)->info, input))
 		return (1);
-	error = pthread_mutex_lock(philo_node->left_fork);
-	if (!error)
-	{
-		t = ft_timestamp_ms() - start_time;
-		ft_lock_print(t, thread_num, "has taken a fork",
-			((t_input *)input)->info);
-	}
-	if (ft_is_dead_full(input))
-	{
-		pthread_mutex_unlock(philo_node->left_fork);
-		return (1);
-	}
-	error = pthread_mutex_lock(philo_node->right_fork);
-	if (!error)
-	{
-		t = ft_timestamp_ms() - start_time;
-		pthread_mutex_lock(philo_node->philo_lock);
-		*(philo_node->times_eat) = *(philo_node->times_eat) + 1;
-		*(philo_node->last_eat) = t;
-		pthread_mutex_unlock(philo_node->philo_lock);
-		ft_lock_print_eating(t, thread_num, ((t_input *)input)->info);
-		ft_wait_ms(eat_time, t, start_time);
-	}
+	if (pthread_mutex_lock(philo_node->right_fork))
+		return (2);
+	t = ft_timestamp_ms() - start_time;
+	pthread_mutex_lock(philo_node->philo_lock);
+	*(philo_node->times_eat) = *(philo_node->times_eat) + 1;
+	*(philo_node->last_eat) = t;
+	pthread_mutex_unlock(philo_node->philo_lock);
+	ft_lock_print_eating(t, thread_num, ((t_input *)input)->info);
+	ft_wait_ms(eat_time, t, start_time);
 	finish = ft_sleep(input, thread_num);
 	if (finish || ft_is_dead_full(input))
 		return (1);
-	return (error);
+	return (0);
 }
