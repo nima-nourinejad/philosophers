d1f611c4cb5d9b4d751431da6c31c429630604a0
Nima Nourinejad <nnourine@c1r2p1.hive.fi>
2024-04-04 13:50:09 +0300

    clean up

--- src/c_files/main.c
+++ src/c_files/main.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 10:23:16 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/04 13:45:12 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:47:58 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ int	main(int argc, char **argv)
 	int			error;
 
 	info = ft_create_info(argc, argv);
-	error = pthread_mutex_lock(info->start_lock);
-	if (error)
+	if (pthread_mutex_lock(info->start_lock))
 		return (ft_clean_failure(info, 0, "Locking start lock"));
 	input = ft_create_input(info->data->value, info);
 	if (!input)
@@ -29,8 +28,7 @@ int	main(int argc, char **argv)
 	thread = ft_create_thread(info->data->value, input);
 	if (!thread)
 		return (ft_clean_failure(info, input, "List of threads"));
-	error = pthread_mutex_unlock((input->info)->start_lock);
-	if (error)
+	if (pthread_mutex_unlock((input->info)->start_lock))
 		return (ft_clean_failure(info, input, "Unlocking start lock"));
 	error = ft_check_dead(info);
 	ft_full_clean(info, input, thread);
